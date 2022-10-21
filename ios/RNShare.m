8198e2e3b092fe374f34b4fb3f25639cd7b6e7c3
Karol Moroz <karol@moroz.dev>
2022-10-21 15:35:14 +0200

    Add RNShare module

--- ios/RNShare.m
+++ ios/RNShare.m
@@ -6,3 +6,10 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <React/RCTBridgeModule.h>
+
+@interface RCT_EXTERN_MODULE(RNShare, NSObject)
+
+RCT_EXTERN_METHOD(open:(NSDictionary *) options)
+
+@end
--- ios/RNShare.swift
+++ ios/RNShare.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import UIKit
+import React
+
+class RNShare : NSObject {
+  @objc static func requiresMainQueueSetup() -> Bool {
+    return false
+  }
+  
+  @objc func open(_ options: NSDictionary) -> Void {
+    DispatchQueue.main.async {
+      self._open(options: options)
+    }
+  }
+
+  private func _open(options: NSDictionary) -> Void {
+    var items = [String]()
+    let message = RCTConvert.nsString(options["message"])
+    
+    if message != "" {
+      items.append(message!)
+    }
+    
+    if items.count == 0 {
+      print("No message to share!")
+      return
+    }
+    
+    let controller = RCTPresentedViewController()
+    let shareController = UIActivityViewController(activityItems: ["Hello React Native"], applicationActivities: nil)
+    
+    shareController.popoverPresentationController?.sourceView = controller?.view
+    
+    controller?.present(shareController, animated: true, completion: nil)
+  }
+}
